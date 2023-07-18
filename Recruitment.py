6b24d571ad465ee6f458a5af7932fdebbd193ca5
HappyYosuaHutahaean <139557906+HappyYosuaHutahaean@users.noreply.github.com>
2023-07-18 14:12:08 +0700

    Add files via upload

---
+++ Recruitment.py
@@ -0,0 +1,39 @@
+
+import random
+import string
+from time import sleep
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import constant
+import utils
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    yield driver
+    
+
+def test_recruitment_candidates(driver):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    randomString = ''.join(random.choice(characters) for i in range(8))
+    driver.find_element(
+        By.NAME, 'username').send_keys("Admin")
+    driver.find_element(
+        By.NAME, 'password').send_keys("admin123"+ Keys.ENTER)
+    driver.find_element(
+        By.XPATH, '//a[@href="/web/index.php/recruitment/viewRecruitmentModule"]').click()
+    driver.find_element(
+    By.XPATH, '//li[@class="oxd-topbar-body-nav-tab"]').click()
+    sleep(3)
+    driver.find_element(
+    By.XPATH, '//button[@class="oxd-icon-button oxd-table-cell-action-space"]').click()
+    sleep(3)
+    driver.find_element(
+    By.XPATH, '//button[@class="oxd-button oxd-button--medium oxd-button--label-danger orangehrm-button-margin"]').click()
+    sleep(3)

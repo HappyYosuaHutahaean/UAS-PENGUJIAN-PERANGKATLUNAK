d6be8a263a2ca58b052596c031f8266ccc2a0dde
HappyYosuaHutahaean <139557906+HappyYosuaHutahaean@users.noreply.github.com>
2023-07-18 14:14:15 +0700

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,15 @@
+# constant variable need for data on the automation test
+USERNAME = '202310062'
+PASSWORD = 'HAPPY YOSUA HUTAHAEAN'
+FILE_TYPE = '.xls'
+# Windows
+# FILE_PATH = 'C:\\Users\\User\\Desktop\\Automation testing lms\\file_test.xls'
+# FILE_PATH_DOWNLOADS = 'C:\\Users\\User\\Downloads\\'
+
+
+# Mac
+FILE_PATH = '/Users/Download/UAS-Automation-Testing/file_test.xls'
+FILE_PATH_DOWNLOADS = '/Users/Downloads/'
+ICS_PATH = '/Users/Download/UAS-Automation-Testing/icalexport.ics'
+
+# py.test --excelreport=report%Y-%M-dT%H%.xls 
